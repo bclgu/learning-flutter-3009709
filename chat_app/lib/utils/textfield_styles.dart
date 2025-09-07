b21e379a903880b9aa7c3c6adcee74f226ba484c
Li Gu <lgu@brightcove.com>
2025-09-07 19:21:12 +1000

    05_04_e

--- chat_app/lib/utils/textfield_styles.dart
+++ chat_app/lib/utils/textfield_styles.dart
@@ -2,6 +2,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 class ThemeTextStyle {
-  static TextStyle loginTextFieldStyle = GoogleFonts.lato(
-      textStyle: TextStyle(color: Colors.blueGrey));
-  }
+  static TextStyle loginTextFieldStyle =
+      GoogleFonts.lato(textStyle: TextStyle(color: Colors.blueGrey));
+}
