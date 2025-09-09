6ba70aa120e2f04c281eb85dc89e04ba700f6902
Li Gu <lgu@brightcove.com>
2025-09-10 06:18:14 +1000

    05_07_e

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -2,6 +2,7 @@ import 'package:chat_app/chat_page.dart';
 import 'package:chat_app/utils/spaces.dart';
 import 'package:chat_app/widgets/login_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_buttons/social_media_buttons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
@@ -110,7 +111,18 @@ class LoginPage extends StatelessWidget {
                     Text(_mainUrl),
                   ],
                 ),
-              )
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialMediaButton.twitter(
+                      size: 20,
+                      color: Colors.blue,
+                      url: "https://twitter.com/pooja_bhaumik"),
+                  SocialMediaButton.linkedin(
+                      url: "https://linkedin.com/in/poojab26"),
+                ],
+              ),
             ],
           ),
         ),
