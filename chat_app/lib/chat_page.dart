29e8067fe247ee2602cec8e01344a48acf3aaada
Li Gu <lgu@brightcove.com>
2025-09-11 14:57:33 +1000

    06_04_b

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
-
 import 'package:chat_app/models/chat_message_entity.dart';
+import 'package:chat_app/models/image_model.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class ChatPage extends StatefulWidget {
   ChatPage({Key? key}) : super(key: key);
@@ -39,9 +40,26 @@ class _ChatPageState extends State<ChatPage> {
     setState(() {});
   }
 
+  _getNetworkImages() async {
+    var endpointUrl = Uri.parse('https://pixelford.com/api2/images');
+
+    final response = await http.get(endpointUrl);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> decodedList = jsonDecode(response.body) as List;
+
+      final List<PixelfordImage> _imageList = decodedList.map((listItem) {
+        return PixelfordImage.fromJson(listItem);
+      }).toList();
+
+      print(_imageList[0].urlFullSize);
+    }
+  }
+
   @override
   void initState() {
     _loadInitialMessages();
+    _getNetworkImages();
     super.initState();
   }
 
