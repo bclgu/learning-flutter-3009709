e155c5c177904d41f3da057063e81b54d3dead25
Li Gu <lgu@brightcove.com>
2025-09-11 16:34:23 +1000

    06_07_b

--- chat_app/lib/widgets/chat_input.dart
+++ chat_app/lib/widgets/chat_input.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/models/chat_message_entity.dart';
+import 'package:chat_app/widgets/picker_body.dart';
 import 'package:flutter/material.dart';
 
 class ChatInput extends StatelessWidget {
@@ -29,7 +30,13 @@ class ChatInput extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return NetworkImagePickerBody();
+                  });
+            },
             icon: Icon(
               Icons.add,
               color: Colors.white,
---
+++ chat_app/lib/widgets/picker_body.dart
@@ -0,0 +1,34 @@
+import 'package:chat_app/models/image_model.dart';
+import 'package:chat_app/repo/image_repository.dart';
+import 'package:flutter/material.dart';
+
+class NetworkImagePickerBody extends StatelessWidget {
+  NetworkImagePickerBody({super.key});
+
+  final ImageRepository _imageRepo = ImageRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<PixelfordImage>>(
+        future: _imageRepo.getNetworkImages(),
+        builder: (BuildContext context,
+            AsyncSnapshot<List<PixelfordImage>> snapshot) {
+          if (snapshot.hasData)
+            return GridView.builder(
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  crossAxisSpacing: 2,
+                  mainAxisSpacing: 2,
+                  maxCrossAxisExtent: MediaQuery.of(context).size.width / 2,
+                ),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(snapshot.data![index].urlSmallSize);
+                });
+          else
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(child: CircularProgressIndicator()),
+            );
+        });
+  }
+}
