952f4869352737dd817b8e4110dd27a64b32bf62
Li Gu <lgu@brightcove.com>
2025-09-11 16:01:31 +1000

    06_06_b

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/models/image_model.dart';
+import 'package:chat_app/repo/image_repository.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
@@ -40,29 +41,11 @@ class _ChatPageState extends State<ChatPage> {
     setState(() {});
   }
 
-  Future<List<PixelfordImage>> _getNetworkImages() async {
-    var endpointUrl = Uri.parse('https://pixelford.com/api2/images');
-
-    final response = await http.get(endpointUrl);
-
-    if (response.statusCode == 200) {
-      final List<dynamic> decodedList = jsonDecode(response.body) as List;
-
-      final List<PixelfordImage> _imageList = decodedList.map((listItem) {
-        return PixelfordImage.fromJson(listItem);
-      }).toList();
-
-      print(_imageList[0].urlFullSize);
-      return _imageList;
-    } else {
-      throw Exception('Failed to load images');
-    }
-  }
+  final ImageRepository _imageRepo = ImageRepository();
 
   @override
   void initState() {
     _loadInitialMessages();
-    _getNetworkImages();
     super.initState();
   }
 
@@ -87,7 +70,7 @@ class _ChatPageState extends State<ChatPage> {
       body: Column(
         children: [
           FutureBuilder<List<PixelfordImage>>(
-              future: _getNetworkImages(),
+              future: _imageRepo.getNetworkImages(),
               builder: (BuildContext context,
                   AsyncSnapshot<List<PixelfordImage>> snapshot) {
                 if (snapshot.hasData)
