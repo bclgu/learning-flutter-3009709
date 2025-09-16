43d47dc57577fc7de2a283f6da5a5090fb2be3cf
Li Gu <lgu@brightcove.com>
2025-09-17 07:03:33 +1000

    07_04_e

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -4,7 +4,9 @@ import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AuthService.init();
   runApp(Provider(
     create: (BuildContext context) => AuthService(),
     child: ChatApp(),
